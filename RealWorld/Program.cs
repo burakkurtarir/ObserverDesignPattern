89ea1458a024b09c441afb450d2f48594683c813
burakkurtarir <burak.kurtarir35@hotmail.com>
2019-07-19 17:17:43 +0300

    Observer Design Pattern

---
+++ RealWorld/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RealWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IBM ibm = new IBM("IBM", 50.00);
+            ibm.Attach(new Investor("Frodo"));
+            ibm.Attach(new Investor("Sam"));
+            ibm.Attach(new Investor("Gandalf"));
+
+            ibm.Price = 100.00;
+            ibm.Price = 120.00;
+        }
+    }
+
+    abstract class Stock
+    {
+        private string _symbol;
+        private double _price;
+        private List<IInvestor> _investors = new List<IInvestor>();
+
+        public Stock(string symbol, double price)
+        {
+            _symbol = symbol;
+            _price = price;
+        }
+
+        public void Attach(IInvestor investor)
+        {
+            _investors.Add(investor);
+        }
+
+        public void Detach(IInvestor investor)
+        {
+            _investors.Remove(investor);
+        }
+
+        public void Notify()
+        {
+            foreach (IInvestor investor in _investors)
+            {
+                investor.Update(this);
+            }
+        }
+
+        public double Price
+        {
+            get { return _price; }
+            set
+            {
+                if (_price != value)
+                {
+                    _price = value;
+                    Notify();
+                }
+            }
+        }
+
+        public string Symbol
+        {
+            get { return _symbol; }
+        }
+    }
+
+    class IBM : Stock
+    {
+        public IBM(string symbol, double price) : base(symbol, price)
+        {
+
+        }
+    }
+
+    interface IInvestor
+    {
+        void Update(Stock stock);
+    }
+
+    class Investor : IInvestor
+    {
+        private string _name;
+        private Stock _stock;
+
+        public Investor(string name)
+        {
+            _name = name;
+        }
+
+        public void Update(Stock stock)
+        {
+            Console.WriteLine($"Notified {_name} of {stock.Symbol}'s change to ${stock.Price}");
+        }
+
+        public Stock stock
+        {
+            get { return _stock; }
+            set { _stock = value; }
+        }
+    }
+}
---
+++ Sample/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteSubject subject = new ConcreteSubject();
+            subject.Attach(new ConcreteObserver(subject, "X"));
+            subject.Attach(new ConcreteObserver(subject, "Y"));
+            subject.Attach(new ConcreteObserver(subject, "Z"));
+
+            subject.SubjectState = "New State";
+            subject.Notify();
+
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Observer
+    {
+        public abstract void Update();
+    }
+
+    class ConcreteObserver : Observer
+    {
+        private string _name;
+        private string _observerState;
+        private ConcreteSubject _subject;
+
+        public ConcreteObserver(ConcreteSubject subject, string name)
+        {
+            _subject = subject;
+            _name = name;
+        }
+        public override void Update()
+        {
+            _observerState = _subject.SubjectState;
+            Console.WriteLine($"Observer {_name}'s new state is {_observerState}");
+        }
+
+        public ConcreteSubject Subject
+        {
+            get { return _subject; }
+            set { _subject = value; }
+        }
+    }
+
+    abstract class Subject
+    {
+        private List<Observer> _observers = new List<Observer>();
+
+        public void Attach(Observer observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Detach(Observer observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (var observer in _observers)
+            {
+                observer.Update();
+            }
+        }
+    }
+
+    class ConcreteSubject : Subject
+    {
+        private string _subjectState;
+
+        //Gets or sets subject state
+        public string SubjectState
+        {
+            get { return _subjectState; }
+            set { _subjectState = value; }
+        }
+    }
+
+
+}
